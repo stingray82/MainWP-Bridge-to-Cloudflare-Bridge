83e43eaeaef09b477ec8003cb39c6dd9e50bef45
Stingray82 <11146912+stingray82@users.noreply.github.com>
2025-07-27 15:59:13 +0100

    Version 1.3.4-alpha.1 Release

--- main-wp-to-cloudflare-bridge-extension/main-wp-to-cloudflare-bridge-extension.php
+++ main-wp-to-cloudflare-bridge-extension/main-wp-to-cloudflare-bridge-extension.php
@@ -5,7 +5,7 @@
  * Tested up to:      6.8.2
  * Requires at least: 6.5
  * Requires PHP:      8.0
- * Version:           1.0.6.21-alpha.1
+ * Version:           1.3.4-alpha.1
  * Author:            Stingray82
  * Author URI:        https://github.com/stingray82
  * License:           GPLv2
@@ -338,7 +338,7 @@ function cfmwp_format_bandwidth($bytes) {
 }
 
 // Define plugin constants
-define('RUP_MAINWP_CLF_BRIDGE_VERSION', '1.0.6.21-alpha.1');
+define('RUP_MAINWP_CLF_BRIDGE_VERSION', '1.3.4-alpha.1');
 
 // ──────────────────────────────────────────────────────────────────────────
 //  Updater bootstrap (plugins_loaded priority 1):
